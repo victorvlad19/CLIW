54bfd5503aac74e24055f3b7b666314aad9a0992
Victor Vlad <victorvlad19@gmail.com>
2018-10-20 12:35:07 +0300

    Added Responsive Hats Grid

--- script.js
+++ script.js
@@ -13,6 +13,18 @@ function closeNavMobile() {
     document.getElementById("fade").style.display = "none";
 }
 
+function Search() {
+    document.getElementById("fade-search").style.display = "block";
+    document.getElementById("clearBtn").style.display = "block";
+    document.getElementById("searchIcon").style.color="#000"
+}
+
+function closeSearch() {
+    document.getElementById("fade-search").style.display = "none";
+    document.getElementById("clearBtn").style.display = "none";
+    document.getElementById("searchIcon").style.color="#A1A1A1"
+}
+
 // Close side-nav if open in Deskop mode
 var mql = window.matchMedia('(max-width: 1024px)');
 function screenTest(e) {
